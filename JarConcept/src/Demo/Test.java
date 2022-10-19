f1edc3b0a19e57039ca4e35f848f46176e7f7ede
sachinsharma263 <sachinsharma263@gmail.com>
2022-10-20 00:00:04 +0530

    first commit

---
+++ JarConcept/src/Demo/Test.java
@@ -0,0 +1,20 @@
+package Demo;
+
+import java.util.Properties;
+
+import com.qa.trcrm.base.BasePage;
+import com.qa.trcrm.base.BasePage2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		BasePage2 bp=new BasePage2();
+		Properties prop=bp.init_prop();
+		
+		BasePage bp2=new BasePage();
+		bp2.init_driver(prop);
+		
+	}
+
+}
